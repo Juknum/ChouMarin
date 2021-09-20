c7f246559a1b739b6530300b260ff62723fde0b4
Jnius0 <rjnius@gmail.com>
2021-09-20 12:54:35 +0200

    Premier commit: set up gitignore+ création du dossier projet éclipse
    gitignore indique d'ignorer les .metadata

---
+++ ramos_jules_submarine/Server.cpp
@@ -0,0 +1,10 @@
+/*
+ * Server.cpp
+ *
+ *  Created on: 20 sept. 2021
+ *      Author: Jules
+ */
+
+
+
+
---
+++ ramos_jules_submarine/Server.hpp
@@ -0,0 +1,15 @@
+/*
+ * Server.hpp
+ *
+ *  Created on: 20 sept. 2021
+ *      Author: Jules
+ */
+
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+
+
+
+
+
+#endif /* SERVER_HPP_ */
